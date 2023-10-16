5a31e2aacbe883f520ec2a9153fb9c85c5195eb4
kotarokomori <komopro.works@gmail.com>
2023-10-17 01:34:40 +0900

    metaデータの追加をしました

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,29 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const siteName = 'WebWorkWords'
+const description =
+  'ウェブにまつわる技術を、直感的に理解でき、業務レベルに引き上げてくれるアプリです。イラストを用いた、わかりやすい言葉の解説と、それを業務レベルに引き上げるクイズで学びたい方はお試しください。'
+
 export const metadata = {
-  title: 'Hello World',
-  description: 'Hello'
+  title: siteName,
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    image: '#',
+    site: '',
+    siteName,
+    locale: 'ja_JP',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    site: '',
+    creator: ''
+  }
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
