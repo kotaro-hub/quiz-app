06a7c71c7ab9617b91c6c257edfd55e2c6888b60
kotarokomori <komopro.works@gmail.com>
2023-10-28 21:12:07 +0900

    バナーの文章を作成

--- src/features/Home/BannerWithAction.tsx
+++ src/features/Home/BannerWithAction.tsx
@@ -1,5 +1,18 @@
+import Button from '@/components/Button'
+import Link from 'next/link'
+
 const BannerWithAction = () => {
-  return <>BannerWithAction</>
+  return (
+    <div>
+      <h2>Webにまつわる技術や用語、わかる言葉で理解する。</h2>
+      <p>
+        技術や用語をイラストと言葉で一つずつ理解し、それらを組み合わせたクイズで、実践的な知識を身につけることができます。
+      </p>
+      <Button>
+        <Link href="/learn">学習する</Link>
+      </Button>
+    </div>
+  )
 }
 
 export default BannerWithAction
