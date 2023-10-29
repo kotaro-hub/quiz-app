39dd02f5d5ba8062f1119ebe3cb420b7cb34ec14
kotarokomori <komopro.works@gmail.com>
2023-10-29 17:16:49 +0900

    アプリの使い方の文章を作成

--- src/features/Home/HowToUseGuide.tsx
+++ src/features/Home/HowToUseGuide.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image'
+
 const HowToUseGuide = () => {
-  return <>HowToUseGuide</>
+  return (
+    <section>
+      <h3>このアプリの使い方</h3>
+      <div>
+        <Image src="#" alt="" />
+        <div>
+          <h4>カードで学ぶ</h4>
+          <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+        </div>
+      </div>
+      <div>
+        <Image src="#" alt="" />
+        <div>
+          <h4>クイズで学ぶ</h4>
+          <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default HowToUseGuide
